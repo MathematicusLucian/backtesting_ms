c5e98404819edad1e1f4de752f4a621cb82d942b
k1n4kut4 <recruitlukejones@gmail.com>
2024-05-24 12:50:08 +0100

    Walkforward Op class

---
+++ src/domains/backtesting_service/w_fwd/walkforward.py
@@ -0,0 +1,67 @@
+import numpy as np
+import pandas as pd
+import vectorbt as vbt
+from src.domains.strategies.ma import simulate_all_params__MA, simulate_best_params__MA
+
+class Walkforward:
+    def __init__(self, asset_data):
+        self.asset_data = asset_data
+        # self.asset_data.vbt.plot().show()
+        self.rolling_in_and_out_sample_config = dict(           # 30 windows, each 2 years long, reserve 180 days for test
+            n=30, 
+            window_len=365 * 2, 
+            set_lens=(180,), 
+            left_to_right=False
+        )  
+        self.simulation_config = dict(                          # Backtesting Portfolio params
+            init_cash=100.,                                     # 100$ initial cash
+            slippage=0.001,                                     # 0.1%
+            fees=0.001,                                         # 0.1%
+            freq='d',
+            # direction='all',  # direction='both',             # long and short
+            # sl_stop=stop,                                     # Stop Loss
+            # sl_trail=True,                                    # Trailing Stop Loss
+        )
+        self.in_sharpe = None
+        self.in_best_index = None
+        self.windows = np.arange(10, 50)
+    
+    def roll_in_and_out_samples(self, price, **kwargs):
+        return price.vbt.rolling_split(**kwargs)
+    
+    def in_hold_sharpe(self):
+        return self.wf.simulate_holding(self.in_price, **self.wf.simulation_config)
+    
+    def calculate_in_hold_sharpe(self):
+        (self.in_price, self.in_indexes), (self.out_price, self.out_indexes) = self.wf.roll_in_and_out_samples(self.asset_data, **self.wf.rolling_in_and_out_sample_config)    
+            # plot=True, trace_names=['in-sample', 'out-sample']  # .show()
+        self.in_hold_sharpe = self.calculate_in_hold_sharpe()
+        self.in_best_fast_windows, self.in_best_slow_windows = self.wf.best_slow_and_fast_windows(self.in_price, self.windows)    # Simulate all params for in-sample ranges
+
+    def in_sample_length(self):
+        return self.in_price.shape, len(self.in_indexes)
+    
+    def out_sample_length(self):
+        return self.out_price.shape, len(self.out_indexes)
+    
+    def calculate_in_best_window_pairs(self):
+        return  np.array(list(zip(self.in_best_fast_windows, self.in_best_slow_windows)))
+        # pd.DataFrame(in_best_window_pairs, columns=['fast_window', 'slow_window']).vbt.plot().show()
+
+    def simulate_all_params(self, strategy, price, windows, **kwargs):
+        if(strategy == 'MA'):
+            return simulate_all_params__MA(self, price, windows, **kwargs)
+        return None
+
+    def calculate_in_sharpe(self, strategy):
+        return self.simulate_all_params(strategy, self.in_price, self.windows, **self.simulation_config)
+    
+    def calculate_out_sharpe(self, strategy):
+        return self.simulate_all_params(strategy, self.out_price, self.windows, **self.simulation_config)
+
+    def simulate_holding(self, strategy, price, **kwargs):
+        in_sharpe = self.calculate_in_sharpe(strategy)
+        out_sharpe = self.calculate_out_sharpe(strategy)
+        pf = vbt.Portfolio.from_holding(price, **kwargs)
+        out_hold_sharpe = pf.sharpe_ratio()
+        return in_sharpe, out_sharpe, out_hold_sharpe
---
+++ src/domains/backtesting_service/w_fwd/walkforward_o.py
@@ -0,0 +1,61 @@
+import numpy as np
+import pandas as pd
+import vectorbt as vbt
+from src.core.asset_data_service.asset_data_service import fetch_asset_data__close
+from src.domains.backtesting_service.w_fwd.walkforward import Walkforward
+
+class Walkforward_Optimisation:
+    def __init__(self, asset_data):
+        self.asset_data = asset_data
+        self.wf = Walkforward(asset_data)
+
+    def simulate_best_params__MA(self, price, best_fast_windows, best_slow_windows, **kwargs):
+        return self.wf.simulate_best_params__MA(price, best_fast_windows, best_slow_windows, **kwargs)
+
+    def get_best_index(self, performance, higher_better=True):
+        if higher_better:
+            return performance[performance.groupby('split_idx').idxmax()].index
+        return performance[performance.groupby('split_idx').idxmin()].index
+
+    def best_slow_and_fast_windows(self, in_price, windows):
+        self.in_sharpe = self.calculate_in_sharpe()
+        self.in_best_index = self.get_best_index(self.in_sharpe)
+        in_best_fast_windows = self.get_best_params(self.in_best_index, 'fast_window')
+        in_best_slow_windows = self.get_best_params(self.in_best_index, 'slow_window')
+        return in_best_fast_windows, in_best_slow_windows
+
+    def get_best_params(self, best_index, level_name):
+        return best_index.get_level_values(level_name).to_numpy()
+    
+    def present_results(self, in_hold_sharpe, out_hold_sharpe, out_test_sharpe):
+        cv_results_df = pd.DataFrame({
+            'in_sample_hold': in_hold_sharpe.values,
+            'in_sample_median': self.in_sharpe.groupby('split_idx').median().values,
+            'in_sample_best': self.in_sharpe[self.in_best_index].values,
+            'out_sample_hold': out_hold_sharpe.values,
+            'out_sample_median': self.out_sharpe.groupby('split_idx').median().values,
+            'out_sample_test': out_test_sharpe.values
+        })
+        color_schema = vbt.settings['plotting']['color_schema']
+        cv_results_df.vbt.plot(
+            trace_kwargs=[
+                dict(line_color=color_schema['blue']),
+                dict(line_color=color_schema['blue'], line_dash='dash'),
+                dict(line_color=color_schema['blue'], line_dash='dot'),
+                dict(line_color=color_schema['orange']),
+                dict(line_color=color_schema['orange'], line_dash='dash'),
+                dict(line_color=color_schema['orange'], line_dash='dot')
+            ]
+        ).show()
+
+if __name__ == "__main__":
+    asset_data = fetch_asset_data__close('BTC-USD')   
+    strategy = 'MA'
+    wfo = Walkforward_Optimisation(asset_data)
+    wfo.in_sample_simulation()
+    # Optimisation
+    in_sharpe, out_sharpe, out_hold_sharpe = wfo.simulate_holding(strategy, wfo.wf.out_price, **wfo.simulation_config)
+    out_sharpe = wfo.calculate_out_sharpe()     # Simulate all params for out-sample ranges
+    out_test_sharpe = wfo.simulate_best_params(wfo.wf.out_price, wfo.wf.in_best_fast_windows, wfo.wf.in_best_slow_windows, **wfo.simulation_config)       
+        # Best in-sample params ranges used to simulate out-sample ranges
+    wfo.present_results(wfo.wf.in_hold_sharpe, out_hold_sharpe, out_test_sharpe)
